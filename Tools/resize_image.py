b53582740e863d22d59cf26f6b56c5737751bc9b
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2019-06-12 19:47:02 +0700

    development

--- Tools/resize_image.py
+++ Tools/resize_image.py
@@ -2,6 +2,7 @@ import cv2
 import os.path
 import sys
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
 def load_path(path):
@@ -16,14 +17,6 @@ def load_path(path):
 
 
 def resize_image(img_src, dimension=(96, 96)):
-    print(load_path(img_src))
-    # img = cv2.imread(load_path(img_src), cv2.IMREAD_UNCHANGED)
-    # resized = cv2.resize(img, dimension, interpolation=cv2.INTER_AREA)
-    # cv2.imshow("Resized image", resized)
-
-
-# Exampleq
-# resize_image('../data_hr/')
-im = cv2.imread("/Tools/the_girl.jpg")
-print(im)
-print(os.path)
+    img = cv2.imread(img_src, cv2.IMREAD_UNCHANGED)
+    resized = cv2.resize(img, dimension, interpolation=cv2.INTER_AREA)
+    print(resized.shape)
---
+++ code_testing.py
@@ -0,0 +1,3 @@
+from Tools.resize_image import resize_image
+
+resize_image('Tools/the_girl.jpg')
